862527327cbf95b1eb01195edb815f86496d325d
Nitin Madhukar <nk2176342@gmail.com>
2020-06-16 01:33:55 +0530

    linked files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Home from './Home';
+import Course from './Coursepage';
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" exact render={props => (
+        <Home />
+      )}/>
+
+      <Route path="/Course" exact render={props => (
+        <Course />
+      )}/>
+    </div>
+  );
+}
+
+export default App;
